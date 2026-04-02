e89c72b113b65ddcd59a1625b5dd5f00da1366b1
Azertyuiop213 <elijakdu13120@gmail.com>
2026-04-02 17:32:59 +0200

    erreurs - try/except ValueError et ZeroDivisionError

--- erreurs.py
+++ erreurs.py
@@ -1,2 +1,8 @@
-# ══ CHAPITRE : GESTION DES ERREURS ══
-# À compléter
+try:
+    nombre = input("Entrez un nombre : ")
+    resultat = 100/float(nombre)
+    print(resultat)
+except ValueError:
+    print("Vous devez entrer un nombre valide.")
+except ZeroDivisionError:
+    print("Le nombre ne peut pas être zéro.")
