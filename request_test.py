36e26c64c6b564ed406a1528d62ea69ccb8cb10c
Azertyuiop213 <elijakdu13120@gmail.com>
2026-04-07 14:46:11 +0200

    requests - verificateur de site en ligne

--- request_test.py
+++ request_test.py
@@ -1,6 +1,15 @@
 import requests
 
-response = requests.get("https://httpbin.org/get")
-data = response.json()
-print(response.status_code)
-print(data["url"])
+# response = requests.get("https://httpbin.org/get")
+# data = response.json()
+# print(response.status_code)
+# print(data["url"])
+try:
+    site = input("Enter a website URL: ")
+    response = requests.get(site)
+    if response.status_code == 200:
+        print("le site est en ligne")
+    else:
+        print(f"Code : {response.status_code}")
+except requests.exceptions.ConnectionError:
+    print("site introuvable")
