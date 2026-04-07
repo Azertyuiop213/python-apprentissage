b309c1f853f9944b21ff561dd61ecbf237b4fbf1
Azertyuiop213 <elijakdu13120@gmail.com>
2026-04-07 10:44:53 +0200

    poo - heritage classe Guerrier

--- poo.py
+++ poo.py
@@ -19,3 +19,23 @@ rex.afficher()
 lucie.afficher()
 rex.ajouter_points(5)
 lucie.ajouter_points(15)
+
+class Personnage:
+    def __init__(self, nom, vie = 100):
+        self.nom = nom
+        self.vie = vie
+
+    def afficher(self):
+        print(f"Personnage : {self.nom} - Vie : {self.vie}")
+
+class Guerrier(Personnage):
+    def __init__(self, nom, vie = 100, force = 10):
+        super().__init__(nom, vie)
+        self.force = force
+
+    def attaquer(self):
+        print(f"{self.nom} attaque avec une force de {self.force}!")
+
+arthur = Guerrier("Arthur", 120, 15)
+arthur.afficher()
+arthur.attaquer()
